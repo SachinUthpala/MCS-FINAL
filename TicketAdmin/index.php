6d336ac6b51b48c732dfb00c71802bbda13489e8
SachinUthpala <gunasekara.s.u.d.a@gmail.com>
2025-12-24 23:41:33 +0530

    123

--- TicketAdmin/index.php
+++ TicketAdmin/index.php
@@ -14,9 +14,9 @@
 
   <link rel="stylesheet" href="./css/style.css">
   <link rel="apple-touch-icon" sizes="180x180" href="../assets/images/favicons/favicon-16x16.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/favicons/favicon-16x16.png">
-
-    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicons/favicon-16x16.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/favicons/favicon-16x16.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicons/favicon-16x16.png">
+  
 </head>
 
 <body>
